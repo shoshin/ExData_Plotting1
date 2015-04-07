7f903068546dc3dc7903531cbb1a71b0757c7705
shoshin <allan.due@gmail.com>
2015-04-07 13:07:09 -0600

    Assignment 1

---
+++ plot1.R
@@ -0,0 +1,34 @@
+
+# The dataset has 2,075,259 rows and 9 columns. A rough estimate of the memory requirements:
+# From the course Programming R, is number of rows * number of columns * 8 bytes
+# so here 2,075,259 * 9 * 8 bytes =  149,418,648/1,048,576 = 142.496727 MB 
+# Not that big really.  :)  So I will read it all in
+
+# Check to see if the data file is available.  If it isn't download from the location and unzip it
+
+location <- "http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+if(!file.exists("household_power_consumption.txt")) {
+	download.file(location,"household_power_consumption.zip")
+	unzip("household_power_consumption.zip")
+	unlink("household_power_consumption.zip")
+}
+
+#Read the data
+read_data <- read.table("household_power_consumption.txt", header=T, sep=";", na.strings="?",stringsAsFactors = FALSE)
+
+#Subset the data for just the days of interest.  Using the format in the original data file.  No need to convert yet.
+my_data <- subset(read_data, subset = (Date == "1/2/2007" | Date == "2/2/2007"))
+
+#remove the larger full data set as we are done with it
+rm(read_data)
+
+#Clean up the Date/Time and put it in the correct format
+my_data$DateTime<- strptime(paste(my_data$Date,my_data$Time), "%d/%m/%Y %H:%M:%S")
+
+#Plot Number 1 
+
+png('plot1.png',width = 480, height = 480)
+
+hist(my_data$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,38 @@
+
+# The dataset has 2,075,259 rows and 9 columns. A rough estimate of the memory requirements:
+# From the course Programming R, is number of rows * number of columns * 8 bytes
+# so here 2,075,259 * 9 * 8 bytes =  149,418,648/1,048,576 = 142.496727 MB 
+# Not that big really.  :)  So I will read it all in
+
+#Check to see if the data file is available.  If it isn't download from the location and unzip it
+
+location <- "http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+if(!file.exists("household_power_consumption.txt")) {
+	download.file(location,"household_power_consumption.zip")
+	unzip("household_power_consumption.zip")
+	unlink("household_power_consumption.zip")
+}
+
+#Read the data
+read_data <- read.table("household_power_consumption.txt", header=T, sep=";", na.strings="?",stringsAsFactors = FALSE)
+
+#Subset the data for just the days of interest.  Using the format in the original data file.  No need to convert yet.
+my_data <- subset(read_data, subset = (Date == "1/2/2007" | Date == "2/2/2007"))
+
+#remove the larger full data set as we are done with it
+rm(read_data)
+
+#Clean up the Date/Time and put it in the correct format
+my_data$DateTime<- strptime(paste(my_data$Date,my_data$Time), "%d/%m/%Y %H:%M:%S")
+
+
+#Plot Number 3
+
+png('plot3.png',width = 480, height = 480)
+
+with(my_data, plot(DateTime,Sub_metering_1, type="l", ylab = "Energy Sub Metering", col="black"))
+points(my_data$DateTime, my_data$Sub_metering_2, type="l", col="red")
+points(my_data$DateTime, my_data$Sub_metering_3, type="l", col="blue")
+legend("topright", lty = 1, col =c("black", "red", "blue"),legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")) # note lty makes it a line in the legend
+
+dev.off()
